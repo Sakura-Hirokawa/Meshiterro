47bc66aa39e1671b9cb8f6e5807b0ac6a2554b44
Sakura-Hirokawa <sk.hirokawa26@gmail.com>
2021-04-10 21:03:44 +0000

    [Start]4-4/model,POstComment

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,6 @@
 #     https: false
 #   )
 # end
+
+
+Refile.secret_key = '424b20b9a993dee8ae0c6c9714f7b7b63d936b419e493435130ee4e83e595cb0aa5b4249b19ef2a9ef33877bf938e08b1cba580888f04b0de208198e88c13bcb'
